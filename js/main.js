152522ebdbdf6585843345d739a6780462bd42cd
Karol <karol.wawer.pl@gmail.com>
2018-05-07 16:44:51 +0200

    dodanie plikow z zadaniem

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var liczbaPierwsza = 8;
+var liczbaDruga = 13;
+function amount(liczbaPierwsza, liczbaDruga) {
+    var suma = liczbaPierwsza + liczbaDruga;
+    return suma;
+    
+}
+console.log(amount(liczbaPierwsza, liczbaDruga));
